1031fcb958a5205651ec9ff6d4fcc5da8f4a53c1
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-25 15:22:22 +0300

    Initial commit

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        try(var context = new AnnotationConfigApplicationContext()){
+
+        }
+    }
+}
