bfa125f289092a4bfd1ac054c25305012d427519
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-25 17:24:55 +0300

    add layers service, repository

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,6 +1,5 @@
 package main;
 
-import beans.MyBean;
 import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -9,12 +8,6 @@ public class Main {
         try (var context = new AnnotationConfigApplicationContext(ProjectConfig.class)) {
 
 
-            MyBean b1 = context.getBean(MyBean.class);
-            MyBean b2 = context.getBean(MyBean.class);
-            MyBean b3 = context.getBean(MyBean.class);
-            System.out.println(b1);
-            System.out.println(b2);
-            System.out.println(b3);
         }
     }
 }
