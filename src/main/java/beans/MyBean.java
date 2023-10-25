5b114c991d1ee3e5323028df67d3a1ea1d74bccc
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-25 15:28:55 +0300

    @Configuration @Bean

--- src/main/java/beans/MyBean.java
+++ src/main/java/beans/MyBean.java
@@ -1,6 +1,8 @@
 package beans;
 
 
+import org.springframework.context.annotation.Bean;
+
 public class MyBean {
     //whatever you want
 }
--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -1,8 +1,13 @@
 package config;
 
+import beans.MyBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ProjectConfig {
-
+    @Bean
+    public MyBean myBean() {
+        return new MyBean();
+    }
 }
